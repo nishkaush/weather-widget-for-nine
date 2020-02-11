b03196087ac09f4a18c7681acacec20ee869c9ca
Nishant Kaushish <nishant@Nishants-iMac.local>
2020-02-11 22:41:09 +1100

    uncommented code

--- src/App.js
+++ src/App.js
@@ -8,52 +8,52 @@ import Loader from "./components/Loader/Loader";
 
 const App = () => {
   const [weather, setWeather] = useState({
-    place: "Chatswood",
-    temp: 59,
-    wind: 28,
-    windDegrees: 107,
-    imgIcon: "10d",
-    description: "Light Rain"
+    // place: "Chatswood",
+    // temp: 59,
+    // wind: 28,
+    // windDegrees: 107,
+    // imgIcon: "10d",
+    // description: "Light Rain"
   });
   const [error, setError] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
   const [title, setTitle] = useState("");
   const [tempType, setTempType] = useState("degrees");
   const [wind, setWind] = useState("on");
 
-  // useEffect(() => {
-  //   navigator.geolocation.getCurrentPosition(
-  //     async result => {
-  //       const weatherData = await fetchWeather(result.coords);
-  //       setWeather(weatherData);
-  //     },
-  //     error => {
-  //       setIsLoading(false);
-  //       console.log("error", error);
-  //       setError("Error while getting your location");
-  //     }
-  //   );
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      async result => {
+        const weatherData = await fetchWeather(result.coords);
+        setWeather(weatherData);
+      },
+      error => {
+        setIsLoading(false);
+        console.log("error", error);
+        setError("Error while getting your location");
+      }
+    );
 
-  //   const fetchWeather = async ({ latitude, longitude }) => {
-  //     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=dd62ec40039697031010d5e893f5581e&units=metric`;
-  //     try {
-  //       let { data } = await axios.get(apiUrl);
-  //       setIsLoading(false);
-  //       return {
-  //         place: data.sys.name,
-  //         temp: data.main.temp,
-  //         wind: data.wind.speed,
-  //          windDegrees:data.wind.deg,
-  //         imgIcon: data.weather[0].icon,
-  //         description: data.weather[0].description
-  //       };
-  //     } catch (error) {
-  //       setIsLoading(false);
-  //       console.log("err in fetching data", error);
-  //       setError("Error in getting weather data");
-  //     }
-  //   };
-  // }, []);
+    const fetchWeather = async ({ latitude, longitude }) => {
+      const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=dd62ec40039697031010d5e893f5581e&units=metric`;
+      try {
+        let { data } = await axios.get(apiUrl);
+        setIsLoading(false);
+        return {
+          place: data.name,
+          temp: parseInt(data.main.temp),
+          wind: data.wind.speed,
+          windDegrees: data.wind.deg,
+          imgIcon: data.weather[0].icon,
+          description: data.weather[0].description
+        };
+      } catch (error) {
+        setIsLoading(false);
+        console.log("err in fetching data", error);
+        setError("Error in getting weather data");
+      }
+    };
+  }, []);
 
   const loadingComp = isLoading && !error && <Loader />;
   const errorComp = !isLoading && error && <ErrorComp error={error} />;
@@ -72,7 +72,6 @@ const App = () => {
           wind={wind}
           setWind={setWind}
         />
-
         <div style={{ height: "200px", border: "0.5px solid #bebebe" }}></div>
         {Object.keys(weather).length && (
           <WeatherDisplay
